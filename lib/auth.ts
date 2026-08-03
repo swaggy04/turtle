563d06ab6c32826a69fe1a9c05fb91c88c41649f
swagatpadhi <litan3104@gmail.com>
2026-08-03 20:41:23 +0530

    add

--- lib/auth.ts
+++ lib/auth.ts
@@ -2,6 +2,7 @@ import { betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
 
 import { prisma } from "@/lib/prisma";
+import { resend } from "@/lib/resend";
 
 export const auth = betterAuth({
   database: prismaAdapter(prisma, {
@@ -10,13 +11,27 @@ export const auth = betterAuth({
 
   emailAndPassword: {
     enabled: true,
+    sendResetPassword: async ({ user, url }) => {
+      await resend.emails.send({
+        from: "Turtle <onboarding@resend.dev>",
+        to: user.email,
+        subject: "Reset your Turtle password",
+        html: `
+    <h2>Reset your password</h2>
+
+    <p>Click the link below to reset your password.</p>
+
+    <a href="${url}">
+      Reset Password
+    </a>
+  `,
+      });
+    },
   },
 
   secret: process.env.BETTER_AUTH_SECRET!,
 
   baseURL: process.env.BETTER_AUTH_URL!,
 
-  trustedOrigins: [
-    "http://localhost:3000",
-  ],
+  trustedOrigins: ["http://localhost:3000"],
 });
