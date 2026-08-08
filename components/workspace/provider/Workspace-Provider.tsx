b1ebe6b17ab37ed9eadc3665f5d8e8ebaf2c4892
swagatpadhi <litan3104@gmail.com>
2026-08-08 15:58:36 +0530

    add

---
+++ components/workspace/provider/Workspace-Provider.tsx
@@ -0,0 +1,26 @@
+"use client";
+
+import { useReducer, type ReactNode } from "react";
+import { initialWorkspaceState } from "./initial";
+import { workspaceReducer } from "./WorkspaceReducer";
+import { WorkspaceContext } from "./WorkspaceContext";
+
+
+interface WorkspaceProviderProps {
+  children: ReactNode;
+}
+
+export function WorkspaceProvider({
+  children,
+}: WorkspaceProviderProps) {
+  const [state, dispatch] = useReducer(
+    workspaceReducer,
+    initialWorkspaceState
+  );
+
+  return (
+    <WorkspaceContext.Provider value={{ state, dispatch }}>
+      {children}
+    </WorkspaceContext.Provider>
+  );
+}
