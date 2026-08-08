389090d34b405ae58fc83e071558bd577f4e139f
swagatpadhi <litan3104@gmail.com>
2026-08-08 16:21:38 +0530

    add

--- app/workspace/page.tsx
+++ app/workspace/page.tsx
@@ -1,20 +1,25 @@
-import { auth } from "@/lib/auth"
-import { headers } from "next/headers"
-import { redirect } from "next/navigation"
-import WorkspaceClient from "@/components/workspace/WorkspaceLayout"
+import { auth } from "@/lib/auth";
+import { headers } from "next/headers";
+import { redirect } from "next/navigation";
+import WorkspaceClient from "@/components/workspace/WorkspaceLayout";
+import { WorkspaceProvider } from "@/components/workspace/provider/Workspace-Provider";
 
-interface workspacePageProps{
-    searchParams:Promise<{prompt?:string,id?:string}>
+interface workspacePageProps {
+  searchParams: Promise<{ prompt?: string; id?: string }>;
 }
-export default async function Workspace({searchParams}:workspacePageProps) {
-    const session = await auth.api.getSession({headers:await headers()})
-    if(!session){
-        redirect("/sign-in")
-    }
+export default async function Workspace({ searchParams }: workspacePageProps) {
+  const session = await auth.api.getSession({ headers: await headers() });
+  if (!session) {
+    redirect("/sign-in");
+  }
 
-    
+  const { prompt } = await searchParams;
 
-    return <WorkspaceClient/>
+  return (
+    <WorkspaceProvider initialPrompt={prompt}>
+      <WorkspaceClient />
+    </WorkspaceProvider>
+  );
 }
 
 // export default Workspace
