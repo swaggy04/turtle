d8545c3c2c2ce2f738ba65b2e78c586aa6c4d5f2
swagatpadhi <litan3104@gmail.com>
2026-08-12 15:28:02 +0530

    fixed scrolling

--- components/workspace/Preview.tsx
+++ components/workspace/Preview.tsx
@@ -1,23 +1,40 @@
 "use client";
 
+import {
+  SandpackProvider,
+  SandpackPreview,
+} from "@codesandbox/sandpack-react";
+
 import { useWorkspace } from "./provider/WorkspaceContext";
 
 export function Preview() {
   const { state } = useWorkspace();
 
+  const files = Object.fromEntries(
+    Object.values(state.files).map((file) => [
+      `/${file.path}`,
+      {
+        code: file.code,
+      },
+    ])
+  );
+
   return (
     <div className="h-full w-full overflow-hidden bg-white">
-      <div className="flex h-full items-center justify-center">
-        <div className="text-center">
-          <p className="text-sm text-gray-500">
-            Preview
-          </p>
-
-          <p className="mt-2 text-xs text-gray-400">
-            {Object.keys(state.files).length} files generated
-          </p>
-        </div>
-      </div>
+      <SandpackProvider
+        template="react"
+        files={files}
+        customSetup={{
+          dependencies: state.dependencies,
+        }}
+      >
+        <SandpackPreview
+          style={{
+            height: "100%",
+            width: "100%",
+          }}
+        />
+      </SandpackProvider>
     </div>
   );
 }
