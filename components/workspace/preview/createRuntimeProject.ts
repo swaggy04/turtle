00ea0ce304c43e7c379b56b49b4c77f549705ae0
swagatpadhi <litan3104@gmail.com>
2026-08-15 16:44:17 +0530

    generatedproj->filesystree

---
+++ components/workspace/preview/createRuntimeProject.ts
@@ -0,0 +1,14 @@
+// Convert a GeneratedProject into a FileSystemTree that WebContainer can mount
+
+import { GeneratedProject } from "@/services/ai-schema";
+
+
+
+
+
+
+export function createRuntimeProject(project:GeneratedProject) {
+    const files = {}
+ 
+}
+
