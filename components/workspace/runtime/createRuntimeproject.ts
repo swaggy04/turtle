36fadce6ea389ebd528ce1f3ed0a05156750dfa4
swagatpadhi <litan3104@gmail.com>
2026-08-17 17:18:24 +0530

    added createruntime

--- components/workspace/runtime/createRuntimeproject.ts
+++ components/workspace/runtime/createRuntimeproject.ts
@@ -1,17 +1,40 @@
-import { GeneratedProject } from "@/services/ai-schema";
-import { FileSystemTree } from "@webcontainer/api";
+import type { FileSystemTree } from "@webcontainer/api";
+import type { GeneratedProject } from "@/services/ai-schema";
 
 export function createRuntimeProject(project: GeneratedProject): FileSystemTree {
-    const tree :FileSystemTree={}
-    for(const files of project.files){
-        const parts = files.path.split("/")
-        let current=tree
-        for(let i=0;i<parts.length-1;i++){
-            const part = parts[i]
-            const isLast = i == parts.length-1
-        }
-    }
-    return{
+  const tree: FileSystemTree = {};
 
+  for (const file of project.files) {
+    const parts = file.path.split("/");
+    let current = tree;
+
+    for (let i = 0; i < parts.length; i++) {
+      const part = parts[i];
+      const isLast = i === parts.length - 1;
+
+      if (isLast) {
+        current[part] = {
+          file: {
+            contents: file.code,
+          },
+        };
+      } else {
+        if (!(part in current)) {
+          current[part] = {
+            directory: {},
+          };
+        }
+
+        const node = current[part];
+
+        if (!("directory" in node)) {
+          throw new Error(`Expected "${part}" to be a directory.`);
+        }
+
+        current = node.directory;
+      }
     }
+  }
+
+  return tree;
 }
