99651928a0ebfad28311a3ed94cc2558d7accb60
swagatpadhi <litan3104@gmail.com>
2026-08-18 16:14:57 +0530

    added dependencies installation files

---
+++ components/workspace/runtime/startDevServer.ts
@@ -0,0 +1,24 @@
+import WebContainerManager from "./webcontainermanager";
+
+
+export async function startDevServer(
+  onLog?: (log: string) => void
+): Promise<string> {
+  const webcontainer = await WebContainerManager.getInstance();
+
+  const devProcess = await webcontainer.spawn("npm", ["run", "dev"]);
+
+  devProcess.output.pipeTo(
+    new WritableStream({
+      write(chunk) {
+        onLog?.(chunk);
+      },
+    })
+  );
+
+  return new Promise((resolve) => {
+    webcontainer.on("server-ready", (_port, url) => {
+      resolve(url);
+    });
+  });
+}
