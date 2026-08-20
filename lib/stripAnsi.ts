4ae548a68bb810d5c9b51cebb518a984c67c0da9
swagatpadhi <litan3104@gmail.com>
2026-08-21 00:44:13 +0530

    cleanup

--- lib/stripAnsi.ts
+++
@@ -1,7 +0,0 @@
-export function stripAnsi(text: string) {
-  return text
-    // Remove ANSI color codes
-    .replace(/\x1B\[[0-9;?]*[ -/]*[@-~]/g, "")
-    // Remove carriage returns
-    .replace(/\r/g, "");
-}
