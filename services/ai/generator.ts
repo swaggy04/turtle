28b3b70a2ed77198c220959546ba5b9a872525b0
swagatpadhi <litan3104@gmail.com>
2026-08-21 01:01:22 +0530

    types changes made for the contract

--- services/ai/generator.ts
+++
@@ -1,6 +0,0 @@
-import { getProvider } from "./registry";
-import { GenerateProjectOptions } from "./provider";
-
-export function generateProject(options: GenerateProjectOptions) {
-  return getProvider(options.provider).generateProject(options);
-}
